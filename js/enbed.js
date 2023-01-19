3434f62b3e02053ab957ec1ce043ed98c33b6713
Hilfig3rStorage <103517108+Hilfig3rStorage@users.noreply.github.com>
2023-01-19 10:42:13 -0600

    Create enbed.js

---
+++ js/enbed.js
@@ -0,0 +1,30 @@
+
+<script>
+//Checking if the page is loaded in an iframe
+
+if(window.self != window.top) {
+
+//Analyzing URL to check embed link
+
+try {
+
+if(window.parent.location.hostname.indexOf("https://sites.google.com/k12.friscoisd.org/tgcofficial/") == -1) {
+
+window.location.href = "https://example.com";
+
+} else {
+
+//iFrame from host site
+
+}
+
+} catch (ex) {
+
+//Unbed blocks embed
+
+window.location.href = "https://example.com/unbed/blocked/index.html";
+
+}
+
+} 
+</script>
